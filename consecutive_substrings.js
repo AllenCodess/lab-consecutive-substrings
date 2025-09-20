c314ddb135aeaab5fc8dac43f48a324cc6d7acc7
AllenCodess <Aph296@protonmail.com>
2025-09-20 00:08:16 -0400

    bigo lab

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,31 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = []
+
+  // outer loop: starting index
+  for (let i = 0; i < string.length; i++) {
+    // inner loop: ending index
+    for (let j = i + 1; j <= string.length; j++) {
+      result.push(string.slice(i, j)) // take substring from i to j
+    }
+  }
+
+  return result
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']")
+  console.log("=>", consecutiveSubstrings('abc'))
 
-  console.log("");
+  console.log("")
 
-  console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("Expecting: ['a']")
+  console.log("=>", consecutiveSubstrings('a'))
+
+  console.log("")
+
+  console.log("Expecting: []")
+  console.log("=>", consecutiveSubstrings('')) // edge case: empty string
 }
 
-module.exports = consecutiveSubstrings;
-
-
+module.exports = consecutiveSubstrings
